e276a63b948856f40d32b02f298ac899a64c616a
vitor <vitor@LDI301>
2025-03-20 12:10:30 -0300

    Add project files.

---
+++ Cassino3/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Cassino3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.btGirar = new System.Windows.Forms.Button();
+            this.tmrGiro = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl1.Location = new System.Drawing.Point(35, 38);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(51, 55);
+            this.lbl1.TabIndex = 0;
+            this.lbl1.Text = "0";
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl2.Location = new System.Drawing.Point(120, 38);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(51, 55);
+            this.lbl2.TabIndex = 1;
+            this.lbl2.Text = "0";
+            // 
+            // lbl3
+            // 
+            this.lbl3.AutoSize = true;
+            this.lbl3.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl3.Location = new System.Drawing.Point(203, 38);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(51, 55);
+            this.lbl3.TabIndex = 2;
+            this.lbl3.Text = "0";
+            // 
+            // btGirar
+            // 
+            this.btGirar.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btGirar.Location = new System.Drawing.Point(45, 129);
+            this.btGirar.Name = "btGirar";
+            this.btGirar.Size = new System.Drawing.Size(209, 80);
+            this.btGirar.TabIndex = 3;
+            this.btGirar.Text = "Girar";
+            this.btGirar.UseVisualStyleBackColor = true;
+            this.btGirar.Click += new System.EventHandler(this.btGirar_Click);
+            // 
+            // tmrGiro
+            // 
+            this.tmrGiro.Tick += new System.EventHandler(this.tmrGiro_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(301, 236);
+            this.Controls.Add(this.btGirar);
+            this.Controls.Add(this.lbl3);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.lbl1);
+            this.Name = "Form1";
+            this.Text = "CASSINOO";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.Button btGirar;
+        private System.Windows.Forms.Timer tmrGiro;
+    }
+}
+
---
+++ Cassino3/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cassino3
+{
+    public partial class Form1 : Form
+    {
+        int[] roleta;
+        int[] ciclos;
+        Label[] tela;
+        Random r;
+        public Form1()
+        {
+            InitializeComponent();
+            roleta = new int[3];
+            ciclos = new int[3];
+            tela = new Label[] { lbl1, lbl2, lbl3 };
+            r = new Random();
+            for (int i = 0; i < roleta.Length; i++)
+            {
+                roleta[i] = r.Next(0, 10);
+                Atualizar(i);
+            }
+        }
+
+        void Atualizar(int i)
+        {
+            tela[i].Text = roleta[i].ToString();
+        }
+
+        private void btGirar_Click(object sender, EventArgs e)
+        {
+            for(int i = 0; i < ciclos.Length; i++)
+            {
+                ciclos[i] = r.Next(1, 21);
+                tela[i].ForeColor = Color.Black;
+            }
+            Array.Sort(ciclos);
+            btGirar.Enabled = false;
+            tmrGiro.Enabled = true;
+        }
+
+        private void tmrGiro_Tick(object sender, EventArgs e)
+        {
+            bool parado = true;
+            for (int i = 0;i < ciclos.Length; i++)
+            {
+                if (ciclos[i] > 0)
+                {
+                    ciclos[i]--;
+                    if (ciclos[i] == 0)
+                    {
+                        tela[i].ForeColor = Color.Red;
+                    }
+                    roleta[i]++;
+                    if (roleta[i] == 10)
+                        roleta[i] = 0;
+                    Atualizar(i);
+                    parado &= false;
+                }
+                else
+                {
+                    parado &= true;
+                }
+            }
+
+            if (parado)
+            {
+                tmrGiro.Enabled = false;
+                btGirar.Enabled = true;
+            }
+        }
+    }
+}
